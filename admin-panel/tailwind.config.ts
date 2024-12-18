3b05fd2720671d376f1bbec15e3283bacc5e0c17
Jalal Musyaffa Ridho <87407830+jalalridho@users.noreply.github.com>
2024-12-18 13:30:37 +0700

    commit2

--- admin-panel/tailwind.config.ts
+++ admin-panel/tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const Config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +9,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        accent: "#0989FF",
+        topHeadingPrimary: "#010f1c",
+        topHeadingSecondary: "#021d35",
+        pink: "#FD4B6B",
+      },
+
+      container: {
+        center: true,
+        padding: "15px",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default Config;
